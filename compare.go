7552908edce6eb343334ebb54b321f03c0ee5ed4
TJ Merritt <github.com@tj.merritts.org>
2019-04-25 21:46:14 -0700

    SetDiffFunc to just DiffFunc. cleanup comments.

--- compare.go
+++ compare.go
@@ -7,12 +7,12 @@ import (
 
 var diffFunc func(out io.Writer, a, b string)
 
-// SetDiffFunc set a function that Compare will use to compute and report differences between two strings
-func SetDiffFunc(f func(out io.Writer, a, b string)) {
+// DiffFunc sets a function that Compare will use to compute and report differences between two strings
+func DiffFunc(f func(out io.Writer, a, b string)) {
 	diffFunc = f
 }
 
-// Compare converts two values to there initialization format and then optionally output a diff betwen the two
+// Compare converts two values to their initialization format and then optionally outputs a diff betwen the two
 // representations if the they are different.  Returns true if the representations of the two values are the same.
 func Compare(a, b interface{}) bool {
 	astr := Value(a)
